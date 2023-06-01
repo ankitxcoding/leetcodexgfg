b8f7d3f7ef12614d4afde660d034aabf1add86a4
ankitxcoding <ankitxcoding@gmail.com>
2023-06-01 10:09:36 +0530

    arithmeticNumber

---
+++ gfg/easy/arithmeticNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        if(C==0 && A==B)
+            return 1;
+        double n=(double)(B-A)/C+1;
+        if(n!=int(n))
+            return 0;
+        return n>0? 1: 0;
+    }
+};
